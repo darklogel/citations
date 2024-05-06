5ae4572c4fe3297b27f7ac4972a40353b2435da5
darklogel <leopoldflores@outlook.com>
2024-05-06 20:02:21 +0200

    création du squelette et exemple de frame

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    title = "ma page"
-    content = "contenu"
-    return render_template('base.html', title=title, content=content)
+def index():
+    title = "Mon site web"
+    
+    # Définissez le nom du fichier CSS à inclure
+    css_filename = "style.css"  # Entrez le nom du fichier CSS ici
+    return render_template('article.html', title=title, css_filename=css_filename)
+
 
 
 
